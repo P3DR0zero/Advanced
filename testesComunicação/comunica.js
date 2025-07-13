6ea8e9c4126332ca8793b87f20f5e7938930cc55
pedro <pedrolucas.fc.santos@gmail.com>
2025-07-12 22:32:00 -0300

    Curiosamente isso está funcionando

--- testesComunicação/comunica.js
+++ testesComunicação/comunica.js
@@ -1,4 +1,7 @@
 import mqtt from 'mqtt';
+import { Keys } from './keys.js';
+var espKey;
+
 export class comunica{
 
 static connect(){
@@ -18,9 +21,10 @@ const client = mqtt.connect("ws://mqtt.ect.ufrn.br:1884/mqtt", {
   client.on("message", (topic, message) => {
     try {
       const data = JSON.parse(message.toString());
-      const toRad = deg => deg * Math.PI / 180;
-      targetY = toRad(parseFloat(data.z) || 0);
-      console.log('📦 Recebido:', data);
+      const isPressed = (data.action==='pressed');
+      Keys.pressed[data.key] = isPressed;
+      espKey=data;
+      console.log('📦 Recebido:', espKey);
     } catch (err) {
       console.error("Erro ao processar MQTT:", err);
     }
